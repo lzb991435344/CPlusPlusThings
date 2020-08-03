bcd07cca8421c59cdaea7b12c5828f07898f0975
lzb991435344 <991435344@qq.com>
2020-08-04 00:07:45 +0800

    C++11new
    c++11

---
+++ 02Cplusplus/03C++11new/problem_01_alias.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+//alias
+//template<typename T>
+//using Vec = vector<T, allocator<T>> Vec;
+
+
+/**
+int main() {
+
+	Vec<int> vec = {3, 4, 5, 6};
+	cout << vec.size()<<endl;
+	cout << vec.capacity() << endl;
+
+	for (auto it = vec.begin(); it != vec.end(); it++) {
+		cout<< " "<< *it <<endl;
+	}
+
+	getchar();
+	system("pause");
+	return 0;
+}*/
---
+++ 02Cplusplus/03C++11new/problem_19_uniform_initialization.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+ // initializer_list<T> 会关联一个array<T,n> 里面元素被编译器逐一分解传给函数
+
+/**
+int main() {
+	int value[]{ 1, 2, 3 };      
+	vector<int> v{ 2, 3, 5 };
+	return 0;
+}*/
